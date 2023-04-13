bc8fa016c011ea14ac38dd93e475c59e2bb34f57
Van Tri <vtri2016@gmail.com>
2023-04-13 20:31:34 +0700

    fix
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 exports.__esModule = true;
-__exportStar(require("./dist"), exports);
+__exportStar(require("./dist/src"), exports);
