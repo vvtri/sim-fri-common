27f8ad8305fa13e3b447412f3a39b391c38e7e78
Van Tri <vtri2016@gmail.com>
2023-04-17 22:22:17 +0700

    fix error import
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- dist/src/configs/typeorm.config.js
+++ dist/src/configs/typeorm.config.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.NamingStrategy = void 0;
-const index_util_1 = require("src/utils/index.util");
 const typeorm_1 = require("typeorm");
+const utils_1 = require("../utils");
 class NamingStrategy extends typeorm_1.DefaultNamingStrategy {
     tableName(targetName, userSpecifiedName) {
-        return userSpecifiedName || (0, index_util_1.camelToSnakeCase)(targetName);
+        return userSpecifiedName || (0, utils_1.camelToSnakeCase)(targetName);
     }
     columnName(propertyName, customName, embeddedPrefixes) {
-        return customName || (0, index_util_1.camelToSnakeCase)(propertyName);
+        return customName || (0, utils_1.camelToSnakeCase)(propertyName);
     }
     joinColumnName(relationName, referencedColumnName) {
-        return `${(0, index_util_1.camelToSnakeCase)(relationName)}_id`;
+        return `${(0, utils_1.camelToSnakeCase)(relationName)}_id`;
     }
 }
 exports.NamingStrategy = NamingStrategy;
