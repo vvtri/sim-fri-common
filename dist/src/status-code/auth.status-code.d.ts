5ca93b134bbb721b5326499ada020405499ac729
Van Tri <vtri2016@gmail.com>
2023-04-19 22:25:41 +0700

    add auth status code
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- dist/src/status-code/auth.status-code.d.ts
+++ dist/src/status-code/auth.status-code.d.ts
@@ -2,5 +2,12 @@ export declare const BASE_STATUS_CODE = 1000;
 declare const AuthStatusCode: {
     readonly INVALID_PASSWORD: number;
     readonly INVALID_EMAIL: number;
+    readonly USER_NOT_FOUND: number;
+    readonly USER_VERIFIED: number;
+    readonly TOO_MANY_VERIFICATION_REQUEST: number;
+    readonly USER_NOT_VERIFIED: number;
+    readonly USER_EXISTED: number;
+    readonly USER_TOKEN_NOT_FOUND: number;
+    readonly INVALID_USER_TOKEN: number;
 };
 export { AuthStatusCode };
--- dist/src/status-code/auth.status-code.js
+++ dist/src/status-code/auth.status-code.js
@@ -5,6 +5,13 @@ exports.BASE_STATUS_CODE = 1000;
 const AuthStatusCode = {
     INVALID_PASSWORD: exports.BASE_STATUS_CODE + 1,
     INVALID_EMAIL: exports.BASE_STATUS_CODE + 2,
+    USER_NOT_FOUND: exports.BASE_STATUS_CODE + 3,
+    USER_VERIFIED: exports.BASE_STATUS_CODE + 4,
+    TOO_MANY_VERIFICATION_REQUEST: exports.BASE_STATUS_CODE + 5,
+    USER_NOT_VERIFIED: exports.BASE_STATUS_CODE + 6,
+    USER_EXISTED: exports.BASE_STATUS_CODE + 7,
+    USER_TOKEN_NOT_FOUND: exports.BASE_STATUS_CODE + 8,
+    INVALID_USER_TOKEN: exports.BASE_STATUS_CODE + 9,
 };
 exports.AuthStatusCode = AuthStatusCode;
 Object.keys(AuthStatusCode).forEach((item) => {
